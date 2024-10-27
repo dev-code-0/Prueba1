9b43dcc1542b96408bdc8b607f0c100ac8216764
dev-code-0 <ballonabjose@email.com>
2024-10-27 17:29:16 -0500

    Modificado el CSS del proyecto

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Prueba1/',
-  plugins: [react()],
+  base: '/Prueba1/',  // Asegúrate de usar el nombre exacto del repositorio
+  plugins: [react()]
 })
