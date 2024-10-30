72d74088200e920bd52695bdccce43288a578723
dev-code-0 <ballonabjose@email.com>
2024-10-29 22:37:12 -0500

    Actualiza URL del backend para el despliegue en Render

--- src/App.jsx
+++ src/App.jsx
@@ -61,7 +61,8 @@ function App() {
     });
 
     try {
-      const response = await fetch('http://localhost:5000/download', {
+      const response = await fetch('https://tiktok-audio-backend.onrender.com/download', {
+
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -113,7 +114,17 @@ function App() {
           closeOnClick: true,
           pauseOnHover: true,
           draggable: true,
-          progress: undefined,
+          progressStyle: {background: '#ff2770'},
+          style: {
+            fontFamily: 'Cascadia Code, sans-serif',
+            background: '#333',
+            boxShadow: '0 8px 20px #ff2770',
+            border: '2px solid #ff2770',
+            color: '#fff',
+            fontSize: '1rem',
+            padding: '15px',
+            borderRadius: '10px'
+          },
         });
       }
     } catch (error) {
@@ -126,7 +137,18 @@ function App() {
         closeOnClick: true,
         pauseOnHover: true,
         draggable: true,
-        progress: undefined,
+        progressStyle: {background: '#ff2770'},
+        icon:<svg viewBox="0 0 24 24" width="100%" height="100%" fill="#ff2770"><path d="M11.983 0a12.206 12.206 0 00-8.51 3.653A11.8 11.8 0 000 12.207 11.779 11.779 0 0011.8 24h.214A12.111 12.111 0 0024 11.791 11.766 11.766 0 0011.983 0zM10.5 16.542a1.476 1.476 0 011.449-1.53h.027a1.527 1.527 0 011.523 1.47 1.475 1.475 0 01-1.449 1.53h-.027a1.529 1.529 0 01-1.523-1.47zM11 12.5v-6a1 1 0 012 0v6a1 1 0 11-2 0z"></path></svg>,
+        style: {
+          fontFamily: 'Cascadia Code, sans-serif',
+          background: '#333',
+          boxShadow: '0 8px 20px #ff2770',
+          border: '2px solid #ff2770',
+          color: '#fff',
+          fontSize: '1rem',
+          padding: '15px',
+          borderRadius: '10px'
+        },
       });
     } finally {
       setIsDownloading(false);
